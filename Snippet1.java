0536f1d3e32b36461f525dba99e2c8db1f584ee4
Tung-Neo <62163195+Tung-Neo@users.noreply.github.com>
2020-03-15 23:08:09 +0700

    Add files via upload

---
+++ Snippet1.java
@@ -0,0 +1,17 @@
+package Sesson4;
+
+public class Snippet1 {
+    /**
+     *@param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int first = 400, second = 700, result;
+        result = first + second;
+
+        //  Evaluates the value of result variable
+        if (result > 1000) {
+            second = second + 100;
+        }
+        System.out.println("The value of second is " + second);
+    }
+}
---
+++ Snippet3.java
@@ -0,0 +1,18 @@
+package Sesson4;
+
+public class Snippet3 {
+    /**
+     *@param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int number = 11, remainder;
+
+        //  % operator to return the remainder of the division
+        remainder = number % 2;
+        if (remainder == 0) {
+            System.out.println("Number is even");
+        } else {
+            System.out.println("Number is odd");
+        }
+    }
+}
